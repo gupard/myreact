0775e60e3736d080d3673fd481e357444642bc61
David w gu <david.w.gu@pwc.com>
2019-06-03 18:38:45 +0800

    add test table for delete function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Table from './components/table'
 class App extends Component {
+  state={
+    characters:[
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+  }
+  removeCharacter=index=>{
+    const {characters}=this.state;
+    this.setState({
+      characters:characters.filter((character,i)=>{
+        return i!==index
+      })
+    })
+  }
   render() {
+    const {characters}=this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Table charactersData={characters} removeCharacter={this.removeCharacter}/>
       </div>
     );
   }
